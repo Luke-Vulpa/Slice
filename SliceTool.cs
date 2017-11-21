547481fecf52c74588997692ee51459b1b4b96cf
Luke Vulpa <lukie@subvulpa.net>
2017-11-21 14:36:01 +0000

    Initial commit

---
+++ SliceTool.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slice
+{
+    public class SliceTool
+    {
+        public SliceTool()
+        {
+            
+        }
+        public void SOME()
+        {
+            Console.WriteLine("meep");
+        }
+    }
+}
